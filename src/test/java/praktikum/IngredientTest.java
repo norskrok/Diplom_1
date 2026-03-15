0606dbad5add5072baaa6026b6c1bc3466caa92f
Islam Kitarov <islamidzeee@yandex.ru>
2026-03-15 20:22:49 +0500

    1D

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,14 +1,35 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {IngredientType.SAUCE, "hot sauce", 100},
+                {IngredientType.FILLING, "cutlet", 200}
+        };
+    }
+
     @Test
     public void ingredientGetMethodsTest() {
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
-        assertEquals(IngredientType.SAUCE, ingredient.getType());
-        assertEquals("hot sauce", ingredient.getName());
-        assertEquals(100, ingredient.getPrice(), 0.001f);
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(type, ingredient.getType());
+        assertEquals(name, ingredient.getName());
+        assertEquals(price, ingredient.getPrice(), 0.001f);
     }
 }
